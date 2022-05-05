554da057a564d51d57d15e96cca55203431330d7
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-05-05 15:59:34 +0200

    lista header array di array

---
+++ laravel-comics/config/header-list.php
@@ -0,0 +1,58 @@
+<?php
+
+    return [
+        [
+            "text" => "CHARACTERS",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "COMICS",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "MOVIE",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "TV",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "GAMES",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "COLLECTIVES",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "VIDEOS",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "FANS",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "NEWS",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "SHOP",
+            "url" => "#",
+            "current" => false,
+        ],
+
+    ];
+
+
+?>
