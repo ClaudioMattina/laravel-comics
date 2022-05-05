e56ff2baf1b3fbeabc552eab9409db92e38623c2
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-05-05 16:56:57 +0200

    footer finito

--- laravel-comics/resources/views/partials/footer.blade.php
+++ laravel-comics/resources/views/partials/footer.blade.php
@@ -1,1 +1,73 @@
-<h1>FOOTER</h1>
+<div id="my-container">
+    <div id="lists-footer-container">
+
+        <div id="links-footer">
+
+            <div class="link">
+
+              <h4>DC COMICS</h4>
+              <ul>
+                  <li>ciao</li>
+              </ul>  
+            </div>
+
+            <div class="link">
+              <h4>DC</h4>
+              <ul>
+                <li>ciao</li>
+              </ul>
+            </div>
+            
+            <div class="link">
+              <h4>SITES</h4>
+              <ul>
+                <li>ciao</li>
+              </ul>
+            </div>
+
+            <div class="link">
+              <h4>SHOP</h4>
+              <ul>
+                <li>ciao</li>
+              </ul>
+            </div>
+
+        </div>
+        <div id="bg-container">
+              <div id="img-footer">
+              </div>
+        </div>
+        
+    </div>
+
+    <div id="grey-container"> 
+        <div id="social-footer-container">
+
+        <div>
+              <div id="my-btn">
+                  <a href="#">SIGN-UP NOW!</a>
+              </div>          
+        </div>
+
+        <div id="icons-social-container">
+            <h3>FOLLOW US</h3>
+            <div class="social-icon">
+                <img src="/images/footer-facebook.png" alt="">
+            </div>
+            <div class="social-icon">
+                <img src="/images/footer-periscope.png" alt="">
+            </div>
+            <div class="social-icon">
+                <img src="/images/footer-pinterest.png" alt="">
+            </div>
+            <div class="social-icon">
+                <img src="/images/footer-twitter.png" alt="">
+            </div>
+            <div class="social-icon">
+                <img src="/images/footer-youtube.png" alt="">
+            </div>
+        </div>
+      </div>
+    </div>
+    
+</div>
