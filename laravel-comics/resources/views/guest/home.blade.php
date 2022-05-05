e8f27ff0c4b489b6f269dd0cfffff33b139058ce
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-05-05 17:32:45 +0200

    main content finito (/)

--- laravel-comics/resources/views/guest/home.blade.php
+++ laravel-comics/resources/views/guest/home.blade.php
@@ -1,7 +1,27 @@
 @extends('guest.layouts.main')
 
 @section('main-content')
+<div id="content-container">
+
+    <div class="my-special-container">
+
+        @foreach ($mainContent as $contentItem)
+        <div class="my-scheda">
+            <div class="img-scheda-container">
+              <img src="{{ $contentItem['thumb'] }}" :alt="serieTitle">
+            </div>
+            <h3> {{$contentItem['series']}} </h3>
+        </div>
+        @endforeach
+        
     
+      </div>
     
+      <div id="my-btn">
+          <a href="#">LOAD MORE</a>
+      </div>
+
+</div>    
+
 
 @endsection
